c54c261c098f512214c968ba6f0e6faf65312639
smurraygit <127979150+smurraygit@users.noreply.github.com>
2023-03-22 08:06:47 +0000

    more python lesson

--- Python/lesson 1.py
+++ Python/lesson 1.py
@@ -1,2 +1,11 @@
-name = input('what is your name?  ')
-print('hello  ' + name)
+# print(type(1/2))
+
+name = 'Saeed'
+age = 36
+
+print ( 'hi ' + name + '. Your are '+ str(age) + ' years old')
+
+print (f' hi  {name}  . Your are {age}   years old')
+
+
+
